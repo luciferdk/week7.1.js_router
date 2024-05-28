3fc24f2ec86142be5a2ab85c560cbb344c1a705a
luciferdk <dksinha0701@gmail.com>
2024-05-28 22:25:24 +0530

    adding lazy commponent for routing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,13 @@
+/*
 // for Dashboard page route
-export function Dashboard() {
+export  function Dashboard() {
+    return <div> Dashboard page 
+    </div> 
+    }
+    */
+
+   // for lazy component Dashboard page route
+export default function Dashboard() {
     return <div> Dashboard page 
     </div> 
     }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,13 @@
+/*
 //for landing page route
-export function Landing() {
+export default function Landing() {
+    return <div> Landing page 
+    </div>
+    }
+*/
+
+    //for lazy component landing page route
+export default function Landing() {
     return <div> Landing page 
     </div>
     }
