79b1d226dddd30786253b883c2982bd9c3c3021a
luciferdk <dksinha0701@gmail.com>
2024-05-29 09:28:16 +0530

    added prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,41 @@ function Appbar() {
 //! +++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // ? prop drilling
+import { useState } from 'react'
+//  import './App.css'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <Count count={count} setCount={setCount} />
+    </div>
+  )
+}
+
+
+function Count({count, setCount}) {
+  return <div>
+    {count}
+    <Buttons count={count} setCount={setCount} />
+  </div>
+}
+
+
+function Buttons({ count, setCount }) {
+  return <div>
+    <button onClick={() => { setCount(count + 1) }}>Increase</button>
+    <button onClick={() => { setCount(count - 1) }}>Decrease</button>
+  </div>
+
+
+}
+
+//  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 
 
 export default App
